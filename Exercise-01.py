d828d2d17849e7c7faf1e9ffcc4aedde78181b99
AntoniusIndraa <antoniusindra@student.uns.ac.id>
2023-03-09 22:00:40 +0700

    Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,38 @@
+#Tugas 1 Praktikum PROKOM
+
+print("~"*55)
+print("\n"+"Nama : Antonius Indra Wicaksono")
+print("NIM  : I0322016"+"\n")
+print("~"*55 + "\n")
+
+print("""
+    Perkenalkan nama saya Antonius Indra Wicaksono 
+dengan NIM I0322016. Saya berasal dari Gunungan, Canan, 
+Wedi, Klaten dan saat ini berdomisili di Karanganyar. 
+Karanganyar merupakan daerah yang cukup ramai, karena 
+di tempat saya tinggal dekat dengan sekolah, dari SD 
+hinggan SMA. Selain itu di tempat tinggal saya berada
+disamping bimbel sehingga cukup ramai. Rumah yang saya 
+tinggali berada sekitar 2km dari alun-alun karanganyar.  
+    Aslinya saya berasal dari Klaten, tepatnya dari 
+Gunungan, Canan, Wedi, Klaten. Di kampung halaman saya 
+masih terdapat banyak sawah. Ketika keluar rumah dan 
+berjalan sebentar, akan terlihat hamparan sawah yang 
+cukup luas. Di kampung halaman saya terasa sunyi dan 
+tidak terlalu ramai karena lumayan jauh dari pusat kota. 
+Di daerah saya masih ada bangunan yang bermodel lama 
+seperti pendopo. Selain itu juga masih banyak ayam yang 
+berkeliaran. Untuk bisa ke kampung halaman saya bisa 
+menggunakan motor hingga bis. Jalan menuju desa saya 
+bisa dikatakan cukup bagus dan datar atau rata. Meskipun 
+Bernama Gunungan, tetapi tidak berada didaerah gunung
+ataupun bukit. 
+    Harapan saya sebagai lulusan Teknik Industri UNS 
+adalah saya bisa menerapkan berbagai ilmu yang telah 
+saya pelajari di jurusan ini dan dapat dipergunakan 
+untuk hal yang baik untuk meningkatkan daya saing 
+bangsa Indonesia di dunia. Saya juga berharap bisa 
+mendapatkan pekerjaan sesuai dengan keahlian saya. 
+""")
+
+print("~"*55 + "\n")
